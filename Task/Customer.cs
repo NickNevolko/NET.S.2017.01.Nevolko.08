857a24ef7808ac7cb0f886af857a0286a0c23bf0
nicknevolko <nikolay.nevolko@gmail.com>
2017-04-10 13:08:27 +0300

    first half of the task

---
+++ Task/Customer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    public class Customer : IFormattable
+    {
+        public string Name { get; set; }
+        public string ContactPhone { get; set; }
+        public decimal Revenue { get; set; }
+
+        public Customer(string name, decimal revenue, string contactPhone)
+        {
+            this.Name = name;
+            this.ContactPhone = contactPhone;
+            this.Revenue = revenue;
+        }
+
+        public override string ToString() => this.ToString("G", CultureInfo.CurrentCulture);
+
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (String.IsNullOrEmpty(format))
+                format = "G";
+
+            if (formatProvider == null)
+                formatProvider = CultureInfo.CurrentCulture;
+
+            switch (format.ToUpperInvariant())
+            {
+                case "G":
+                case "NCR": return string.Format(formatProvider, "Customer record: {0}, {1:N}, {2}", Name, Revenue, ContactPhone);
+                case "N":   return string.Format(formatProvider, "Customer record: {0}", Name);
+                case "R":   return string.Format(formatProvider, "Customer record: {0:N}", Revenue);
+                case "C":   return string.Format(formatProvider, "Customer record: {0}", ContactPhone);
+                case "NR":  return string.Format(formatProvider, "Customer record: {0}, {1:N}", Name, Revenue);
+                case "NC":  return string.Format(formatProvider, "Customer record: {0}, {1}", Name, ContactPhone);
+                case "RC":  return string.Format(formatProvider, "Customer record: {0:N}, {1}", Revenue, ContactPhone);
+                default:    throw new FormatException(string.Format($"The format {format} doesn't exist."));
+            }
+
+        }
+    }
+}
---
+++ TaskTests/TestClass.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using Task;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace TaskTests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [TestCase("Jeffrey Richter", 1000000, "+1 (425) 555-0100", "", ExpectedResult = "Customer record: Jeffrey Richter, 1,000,000.00, +1 (425) 555-0100")]
+        [TestCase("Jeffrey Richter", 1000000, "+1 (425) 555-0100","G", ExpectedResult = "Customer record: Jeffrey Richter, 1,000,000.00, +1 (425) 555-0100")]
+        [TestCase("Jeffrey Richter", 1000000, "+1 (425) 555-0100", "N", ExpectedResult = "Customer record: Jeffrey Richter")]
+        [TestCase("Jeffrey Richter", 1000000, "+1 (425) 555-0100", "R", ExpectedResult = "Customer record: 1,000,000.00")]
+        [TestCase("Jeffrey Richter", 1000000, "+1 (425) 555-0100", "C", ExpectedResult = "Customer record: +1 (425) 555-0100")]
+        [TestCase("Jeffrey Richter", 1000000, "+1 (425) 555-0100", "NR", ExpectedResult = "Customer record: Jeffrey Richter, 1,000,000.00")]
+        [TestCase("Jeffrey Richter", 1000000, "+1 (425) 555-0100", "NC", ExpectedResult = "Customer record: Jeffrey Richter, +1 (425) 555-0100")]
+        [TestCase("Jeffrey Richter", 1000000, "+1 (425) 555-0100", "RC", ExpectedResult = "Customer record: 1,000,000.00, +1 (425) 555-0100")]
+        public string Customer_IFormattable_Test(string name, decimal revenue, string contactPhone, string formatProv)
+        {
+            return new Customer(name, revenue, contactPhone).ToString(formatProv, CultureInfo.InvariantCulture);
+        }
+    }
+}
